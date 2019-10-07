7df20460e13917df422d98c4485e67cd3c0c5040
JesKate1993 <jeskate1993@gmail.com>
2019-10-07 08:41:41 -0500

    algo es algo

--- app/src/main/java/com/example/sqlite/Singup.java
+++ app/src/main/java/com/example/sqlite/Singup.java
@@ -14,27 +14,35 @@ import com.example.sqlite.clases.coneccionDB;
 
 public class Singup extends AppCompatActivity {
 
-    private EditText fname;
+    private EditText fname, lapellido, ecorreo, pcontraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
 
+        //obtener id's
         fname=findViewById(R.id.idfname);
+        lapellido=findViewById(R.id.apellido);
+        ecorreo=findViewById(R.id.correo);
+        pcontraseña=findViewById(R.id.contrasena);
+
     }
     public void Singup(View view){
         //coneccion db
-        coneccionDB connection= new coneccionDB(this, "admin", null, 1);
+        coneccionDB connection= new coneccionDB(this, "connection", null, 1);
         //let Db Read-write
         SQLiteDatabase market = connection.getWritableDatabase();
         //get data
         String Fname= fname.getText().toString();
+        String Lapellido= lapellido.getText().toString();
+        String Ecorreo= ecorreo.getText().toString();
+        String Pcontraseña= pcontraseña.getText().toString();
         //save data-- guardar los datos
         ContentValues pack= new ContentValues();
         pack.put("firsname",Fname);
-        pack.put("lastname","");
-        pack.put("email","");
-        pack.put("password","");
+        pack.put("lastname",Lapellido);
+        pack.put("email",Ecorreo);
+        pack.put("password",Pcontraseña);
         market.insert("users", null, pack);
         Toast.makeText(this, "The user has registered", Toast.LENGTH_SHORT).show();
 
